d94934ea994b31bc657f1c0cdffe035a9d5e7b39
ValeriaSihr <132007746+ValeriaSihr@users.noreply.github.com>
2023-11-18 15:48:52 +0000

    task_02

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const ulGallery = document.querySelector('.gallery');
+
+ulGallery.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+function createMarkup(items) {
+  return items
+    .map(
+      item =>
+        `<li class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" alt="${item.description}" /></a></li>`
+    )
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoomFactor: false,
+});
